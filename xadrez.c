8359cbc314725a8bd4bc4657306a2481b24fd447
Talles Andrade Silva <157542214+TallesAndrade@users.noreply.github.com>
2025-03-23 19:15:31 +0000

    desafio nivel aventureiro concluido

--- xadrez.c
+++ xadrez.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int i;
 
     // Movimento da Torre: 5 casas para a direita
     printf("Movimento da Torre:\n");
-    for (i = 1; i <= 5; i++) {
+    for (i = 1; i <= 5; i++)
+    {
         printf("Direita\n");
     }
 
     // Movimento do Bispo: 5 casas na diagonal para cima e à direita
     printf("\nMovimento do Bispo:\n");
     i = 1;
-    while (i <= 5) {
+    while (i <= 5)
+    {
         printf("Cima, Direita\n");
         i++;
     }
@@ -20,10 +23,24 @@ int main() {
     // Movimento da Rainha: 8 casas para a esquerda
     printf("\nMovimento da Rainha:\n");
     i = 1;
-    do {
+    do
+    {
         printf("Esquerda\n");
         i++;
     } while (i <= 8);
+
+    // Movimento do Cavalo: 2 casa para baixo 1 casa para esqueda
+    printf("\nMovimento do Cavalo:\n");
+    for (int i = 0; i < 1; i++)
+    {
+        int j = 0;
+        while (j <= 1)
+        {
+            printf("Baixo\n");
+            j++;
+        }
+        printf("Esquerda\n");
+    }
 
     return 0;
 }
